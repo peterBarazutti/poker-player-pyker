37e45669923f190d4f5ab5a4a6aa496adb65e90a
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-14 11:35:25 +0200

    removed shit

--- player.py
+++ player.py
@@ -3,9 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print game_state["players"][0]["hole_cards"][0]["rank"]
-        print game_state["players"][0]["hole_cards"][1]["rank"]
-        print game_state["players"]
+
         if self.isHighCards() or self.isPair():
             self.allIn()
         else:
