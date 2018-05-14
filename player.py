728f77ef051618bed57a513316ac19aa9e3f7c9b
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-14 10:32:01 +0200

    check loggin

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print("check logging")
         return 1000
 
     def showdown(self, game_state):
