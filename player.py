9e874628eab023ad3bdad88b0667b62c917e5ffb
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-05-14 11:30:15 +0200

    Fix the json parse

--- player.py
+++ player.py
@@ -13,10 +13,10 @@ class Player:
 
     def get_cards(self, game_state):
         returning_string = ""
-        card1_suit = game_state["players"]["hole_cards"][0]["suit"]
-        card2_suit = game_state["players"]["hole_cards"][1]["suit"]
-        card1_rank = game_state["players"]["hole_cards"][0]["rank"]
-        card2_rank = game_state["players"]["hole_cards"][1]["rank"]
+        card1_suit = game_state["players"][2]["hole_cards"][0]["suit"]
+        card2_suit = game_state["players"][2]["hole_cards"][1]["suit"]
+        card1_rank = game_state["players"][2]["hole_cards"][0]["rank"]
+        card2_rank = game_state["players"][2]["hole_cards"][1]["rank"]
         if card1_rank == "10":
             card1_rank = "T"
         if card2_rank == "10":
