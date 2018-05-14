0abffd145ac7422e7d8f9be32995bab055623fcc
Ferenc Bolla <bollaferenc@gmail.com>
2018-05-14 10:57:00 +0200

    log players

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ class Player:
     def betRequest(self, game_state):
         print game_state["players"][0]["hole_cards"][0]["rank"]
         print game_state["players"][0]["hole_cards"][1]["rank"]
+        print game_state["players"]
         return 1000
 
     def showdown(self, game_state):
