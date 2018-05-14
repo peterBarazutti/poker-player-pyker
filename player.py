1e37638518019ab97ddcfe6cb000e899b381439b
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-14 10:46:15 +0200

    print3

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
 
     def betRequest(self, game_state):
         print("check logging")
-        print("check logging2")
+        print "check logging2"
         return 1000
 
     def showdown(self, game_state):
