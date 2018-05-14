926093bb08bbc5a5df8bd2cb5709096c21be8727
Márk Szentmiklósi <szentmiklosi.mark@gmail.com>
2018-05-14 10:53:57 +0200

    printing card values

--- player.py
+++ player.py
@@ -3,8 +3,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print("check logging")
-        print "check logging2"
+        print game_state["players"][0]["hole_cards"][0]["rank"]
+        print game_state["players"][0]["hole_cards"][1]["rank"]
         return 1000
 
     def showdown(self, game_state):
