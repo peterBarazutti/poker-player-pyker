a1136b62d2e6305df61e1733df25adc7005f70ce
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-05-14 10:55:07 +0200

    Create get cards function

--- player.py
+++ player.py
@@ -10,4 +10,20 @@ class Player:
     def showdown(self, game_state):
         pass
 
+    def get_cards(self, game_state):
+        returning_string = ""
+        card1_suit = game_state["players"]["hole_cards"][0]["suit"]
+        card2_suit = game_state["players"]["hole_cards"][1]["suit"]
+        card1_rank = game_state["players"]["hole_cards"][0]["rank"]
+        card2_rank = game_state["players"]["hole_cards"][1]["rank"]
+        if card1_rank == "10":
+            card1_rank = "T"
+        if card2_rank == "10":
+            card2_rank = "T"
+        if card1_suit == card2_suit:
+            returning_string = "S"
+        else:
+            returning_string = "O"
+        returning_string = returning_string + card1_rank + card2_rank
+        return returning_string
 
